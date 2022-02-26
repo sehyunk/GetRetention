8eddcfd62839a76c89cfd986c5ba3f1ed1f090be
Sehyun_Kim <nanica9191@naver.com>
2022-02-26 16:42:01 +0900

    Add files via upload

---
+++ GetRetention.py
@@ -0,0 +1,36 @@
+import pandas as pd
+from datetime import timedelta
+
+def get_cohort_table(df):
+    df = pd.merge(df,df.groupby(['user_id'])[['date']].min(), on ='user_id', how = 'inner')
+    df.columns = ['user_id', 'date', 'first_date']
+    first_order_month = pd.to_datetime(df['first_date']).dt.year*12 + pd.to_datetime(df['first_date']).dt.month
+    order_month = pd.to_datetime(df['date']).dt.year*12 + pd.to_datetime(df['date']).dt.month
+    df['cohort'] = order_month - first_order_month
+    df['first_month'] = df['first_date'].str[:7]
+    return df
+
+def get_cohort(df):
+    df = get_cohort_table(df)
+    cohort = df.groupby(['first_month','cohort'])[['user_id']].nunique().reset_index()
+    cohort = cohort.pivot(index = 'first_month', columns = 'cohort' , values = 'user_id')
+    cohort = (cohort.T/cohort[0]).T*100
+    return cohort
+
+def get_cohort_nunique(df):
+    df = get_cohort_table(df)
+    cohort = df.groupby(['first_month','cohort'])[['user_id']].nunique().reset_index()
+    cohort = cohort.pivot(index = 'first_month', columns = 'cohort' , values = 'user_id')
+    return cohort
+
+def get_retention(df):
+    cohort_nunique = get_cohort_nunique(df)
+    retention = pd.DataFrame()
+    retention.loc['retention', 0] = 100
+
+    for i in cohort_nunique.columns[1:]:
+        i_cohort = cohort_nunique.loc[cohort_nunique[i].notna(),[0,i]].sum()
+        ret_ratio = i_cohort[i]/i_cohort[0]*100
+        retention.loc['retention', i] = ret_ratio
+        
+    return retention
